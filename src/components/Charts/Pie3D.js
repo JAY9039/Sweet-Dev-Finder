a6a44c94cad65f9f1090078f0d2188f7b0c01b30
Jay Sharma <Mailtojaysharma@gmail.com>
2022-08-02 18:46:30 +0530

    added bar column pie charts

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,6 +1,7 @@
 // STEP 1 - Include Dependencies
 // Include react
 import React from "react";
+
 // Include the react-fusioncharts component
 import ReactFC from "react-fusioncharts";
 
@@ -8,52 +9,51 @@ import ReactFC from "react-fusioncharts";
 import FusionCharts from "fusioncharts";
 
 // Include the chart type
-import Column2D from "fusioncharts/fusioncharts.charts";
+import Chart from "fusioncharts/fusioncharts.charts";
 
 // Include the theme as fusion
 import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
 // Adding the chart and theme as dependency to the core fusioncharts
-ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
-
-// STEP 2 - Chart Data
-const chartData = [
-  {
-    label: "HTML",
-    value: "10"
-  },
-  {
-    label: "CSS",
-    value: "40"
-  },
-  {
-    label: "JS",
-    value: "50"
-  },
-];
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 
 // STEP 3 - Creating the JSON object to store the chart configurations
-const chartConfigs = {
-  type: "pie3d", // The chart type
-  width: "400", // Width of the chart
-  height: "400", // Height of the chart
-  dataFormat: "json", // Data type
-  dataSource: {
-    // Chart Configuration
-    chart: {
-      caption: "Languages",
-      theme: "fusion",
-      decimals: 0,
-      pieRadius: '45%',
-      // paletteColors: "#f0db4f",  
-    },
-    // Chart Data
-    data: chartData
-  }
-};
 
-const ChartComponent = () => {
-  return <ReactFC {... chartConfigs} />;
-}
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "pie2d", // The chart type
+    width: "100%", // Width of the chart
+    height: "350", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Languages",
+        captionFontColor: "#102a42",
+        captionFontBold: 0,
+        captionFontSize: 20,
+        captionFont: "Roboto",
+        baseFont: "Open Sans",
+        baseFontSize: 16,
+        baseFontColor: "#617d98",
+        smartLineColor: "#617d98",
+        showShadow: 0,
+        showPlotBorder: 0,
+        paletteColors:
+          "#2caeba, #5D62B5, #FFC533, #F2726F, #8d6e63, #1de9b6, #6E80CA",
+        use3DLighting: 0,
+        useDataPlotColorForLabels: 0,
+        bgColor: "#FFFFFF",
+        showBorder: 0,
+        decimals: 0,
+        pieRadius: "45%",
+      },
+      // Chart Data
+      data,
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
+};
 
 export default ChartComponent;
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,52 +1,90 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
-
+import React from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 
 const Repos = () => {
-
-  const {repos} = React.useContext(GithubContext);
+  const { repos } = React.useContext(GithubContext);
   // console.log(repos);
 
-  let languages = repos.reduce((total, item) =>{
-    const {language}  = item;
+  let languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
     // here we get the no. of instances of languages used in diff. repos
     // sometimes there is a null value so to avoid this we do
-    if(!language == null){
-      return total;
-    }
+    if (!language) return total;
     // but if it is not null
     // this gives us the total count of languages used
     // if the language property is not an object we create a new object of language property
-    if(!total[language]){
-      total[language] = {label: language, value: 1};
+    if (!total[language]) {
+      total[language] = { label: language, value: 1, stars: stargazers_count };
     }
-     // if the language is already on the object
-    else{
-      total[language] = {...total[language], value: total[language].value + 1};
+    // if the language is already on the object
+    else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
+      };
     }
     return total;
-  },{})
+  }, {});
+  // console.log(languages)
 
-  languages = Object.values(languages).sort((a,b) => {
+  const mostUsed = Object.values(languages)
     // this will return the highest language number
-    return b.value - a.value;
-  }).slice(0, 5); // gets the first 5 popular languages
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5); // gets the first 5 popular languages
 
-  console.log(languages);
-    
+  //most starts per language
+
+  const mostPopular = Object.values(languages)
+    .sort((a, b) => {
+      return b.stars - a.stars; // now we sort them bases on stars
+    })
+    .map((item) => {
+      return { ...item,
+        value: item.stars 
+      }; // since the chart is getting updated by values so we replace starts to values
+    })
+    .slice(0, 5); // gets the first 5 popular languages
+
+  // console.log(mostPopular);
+  // console.log(languages);
   // console.log(item);
-   return(
+  // STEP 2 - ChartData
 
-     <section className="section">
-      <Wrapper className="section-center">
-      {/* <ExampleChart data = {chartData} />; */}
-      <Pie3D data = {languages} />;
-      </Wrapper> 
-   </section>
-   )
-   };
+    
+  // stars, forks
+
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+      return total;
+    },
+    {
+      stars: {},
+      forks: {},
+    }
+  );
+
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
+
+return (
+  <section className='section'>
+    <Wrapper className='section-center'>
+      <Pie3D data={mostUsed} />
+      <Column3D data={stars} />
+      <Doughnut2D data={mostPopular} />
+      <Bar3D data={forks} />
+    </Wrapper>
+  </section>
+);
+};
 
 const Wrapper = styled.div`
   display: grid;
@@ -55,11 +93,9 @@ const Wrapper = styled.div`
   @media (min-width: 800px) {
     grid-template-columns: 1fr 1fr;
   }
-
   @media (min-width: 1200px) {
     grid-template-columns: 2fr 3fr;
   }
-
   div {
     width: 100% !important;
   }
